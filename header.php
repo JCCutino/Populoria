45f1f24ecc0b833e2b97165b53cb88b67676fd1d
Rafael Pedrosa <blas_tois@alumnos.ilernasevilla.com>
2024-02-08 19:17:20 +0100

    Header separado y vista cambiada

---
+++ header.php
@@ -0,0 +1,17 @@
+<header class="p-3 text-bg-info">
+    <div class="container">
+        <div class="d-flex flex-wrap align-items-center justify-content-around w-100">
+
+            <p>Buscar proyectos</p>
+            <p>Mis proyectos</p>
+
+            <a href="" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none icon">
+                <img src="image/Populoria.svg" alt="Populoria-Logo" class="img-fluid icon">
+            </a>
+
+            <p>Perfil</p>
+            <p>Cerrar sesión</p>
+
+        </div>
+    </div>
+</header>
--- proyectos.php
+++ proyectos.php
@@ -19,28 +19,7 @@
     </head>
 
     <body>
-        <header class="p-3 text-bg-dark">
-            <div class="container">
-              <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-                <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
-                 <h7>aqui va el logo de la empresa</h7>
-                </a>
-        
-                <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-                  <li><a href="#" class="nav-link px-2 text-secondary">Home</a></li>
-                  <li><a href="#" class="nav-link px-2 text-white">Features</a></li>
-                  <li><a href="#" class="nav-link px-2 text-white">Pricing</a></li>
-                  <li><a href="#" class="nav-link px-2 text-white">FAQs</a></li>
-                  <li><a href="#" class="nav-link px-2 text-white">About</a></li>
-                </ul>
-        
-                <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
-                  <input type="search" class="form-control form-control-dark text-bg-dark" placeholder="Search..." aria-label="Search">
-                </form>
-        
-              </div>
-            </div>
-          </header>
+        <?php @include("header.php"); ?>
         <main>
             <section>
             <aside>
@@ -53,7 +32,7 @@
         </section>
         <section>
             <h3>Colaboradores actuales</h3>
-            <p>(Mostrar los usuarios que colaboran y su foto</p>
+            <p>Mostrar los usuarios que colaboran y su foto</p>
         </section>
         </main>
         <footer>
