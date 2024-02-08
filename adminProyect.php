8143132ba5b9178bf7a039567fd5d1a9c9a9e8e2
Rafa <blas_tois@alumnos.ilernasevilla.com>
2024-02-08 21:57:11 +0100

    Rafa completado

---
+++ adminProyect.php
@@ -0,0 +1,90 @@
+<!doctype html>
+<html lang="es" class="h-100">
+
+<head>
+    <title>Proyecto</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
+    <script defer src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+
+    <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="styles/style.css">
+
+    <link rel="stylesheet" href="styles/style.css">
+
+    <link rel="shortcut icon" href="image/Populoria.svg" type="image/x-icon">
+</head>
+
+<body class="h-100">
+    <?php @include("header.php"); ?>
+    <main class="h-100">
+
+        <article class="d-flex w-100 h-100 proyect-article">
+            <section class="requests w-50 h-75 bg-color-gray mt-5 mx-5 d-flex flex-column gap-5">
+                <h1 class="mt-3">Solicitudes</h1>
+                <div class="d-flex w-100 justify-content-around align-items-center">
+                    <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false">
+                        <title>Placeholder</title>
+                        <rect width="100%" height="100%" fill="#777"></rect><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text>
+                    </svg>
+                    <p>Nombre</p>
+                    <button class="badge rounded-pill bg-success h-100">👁️</button>
+                </div>
+                <div class="d-flex w-100 justify-content-around align-items-center">
+                    <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false">
+                        <title>Placeholder</title>
+                        <rect width="100%" height="100%" fill="#777"></rect><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text>
+                    </svg>
+                    <p>Nombre</p>
+                    <button class="badge rounded-pill bg-success h-100">👁️</button>
+                </div>
+                <div class="d-flex w-100 justify-content-around align-items-center">
+                    <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false">
+                        <title>Placeholder</title>
+                        <rect width="100%" height="100%" fill="#777"></rect><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text>
+                    </svg>
+                    <p>Nombre</p>
+                    <button class="badge rounded-pill bg-success h-100">👁️</button>
+                </div>
+                <div class="d-flex w-100 justify-content-around align-items-center">
+                    <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false">
+                        <title>Placeholder</title>
+                        <rect width="100%" height="100%" fill="#777"></rect><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text>
+                    </svg>
+                    <p>Nombre</p>
+                    <button class="badge rounded-pill bg-success h-100">👁️</button>
+                </div>
+            </section>
+            <section class="w-50 h-75 d-flex flex-column gap-5 mt-5 mx-5">
+                <div class="proyect-title-admin h-50 bg-color-gray">
+                    <h1 class="mt-3">Título del proyecto</h1>
+                    <p>Descripción del proyecto...</p>
+                </div>
+                <div class="proyect-collab-admin h-50 bg-color-gray">
+                    <h1 class="mt-3">Colaboradores actuales</h1>
+                    <div class="d-flex justify-content-center align-items-center h-100 gap-5">
+                        <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false">
+                            <title>Placeholder</title>
+                            <rect width="100%" height="100%" fill="#777"></rect><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text>
+                        </svg>
+                        <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false">
+                            <title>Placeholder</title>
+                            <rect width="100%" height="100%" fill="#777"></rect><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text>
+                        </svg>
+                        <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false">
+                            <title>Placeholder</title>
+                            <rect width="100%" height="100%" fill="#777"></rect><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text>
+                        </svg>
+                    </div>
+                </div>
+            </section>
+        </article>
+
+    </main>
+
+
+</body>
+
+</html>
